9c8d8466d73bc4f32742dddb11009e27f81b7e18
ZERONE <zerone012@gmail.com>
2023-06-26 10:31:26 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/study_flutter_08_codingchef08_json/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.study_flutter_08_codingchef08_json
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/json_parse.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'user.dart';
+import 'services.dart';
+
+class JsonParse extends StatefulWidget {
+  const JsonParse({super.key});
+
+  @override
+  State<JsonParse> createState() => _JsonParseState();
+}
+
+class _JsonParseState extends State<JsonParse> {
+  List<User> _user = <User>[];
+  bool loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Services.getInfo().then((value) {
+      setState(() {
+        _user = value;
+        loading = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loading ? 'User' : 'loading...'),
+      ),
+      body: ListView.builder(
+          itemCount: _user.length,
+          itemBuilder: (context, index) {
+            User user = _user[index];
+            return ListTile(
+              leading: const Icon(
+                Icons.account_circle_rounded,
+                color: Colors.blue,
+              ),
+              trailing: const Icon(
+                Icons.phone_android_rounded,
+                color: Colors.red,
+              ),
+              title: Text(user.name),
+              subtitle: Text(user.email),
+            );
+          }),
+    );
+  }
+}
